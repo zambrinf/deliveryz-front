43985249c072c9cb8e5bdab4acf0c6553c708eea
Felipe Zambrin <zambrinf@gmail.com>
2021-01-07 16:29:32 -0300

    integração backend

--- src/Orders/Orders.tsx
+++ src/Orders/Orders.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchProducts } from "../api";
 import "./orders-styles.css";
+import ProductList from "./ProductList";
 import StepsHeader from "./StepsHeader";
+import { Product } from "./types";
 
 export default function Orders() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetchProducts()
+      .then((response) => setProducts(response.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="orders-container">
       <StepsHeader />
+      <ProductList products={products} />
     </div>
   );
 }
